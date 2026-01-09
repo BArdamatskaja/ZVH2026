5cf7caf49e4d16650693b3bf3746474eb272ebb5
BArdamatskaja <malachovskaja123@gmail.com>
2026-01-09 18:08:58 +0200

    Create backend folder
    Create folder for backend by Spring Initializr

---
+++ Books_Project_Backend/Books_Project/src/test/java/lt/techin/Books_Project/BooksProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.Books_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

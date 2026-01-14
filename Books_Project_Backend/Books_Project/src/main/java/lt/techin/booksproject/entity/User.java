36c91a911105fd4917bcd4a10a85dd40b95aebb9
Algimantas Vizgirda <algimantas.vizgirda@gmail.com>
2026-01-14 14:28:54 +0200

    feat(user): add User entity

---
+++ Books_Project_Backend/Books_Project/src/main/java/lt/techin/booksproject/entity/User.java
@@ -0,0 +1,28 @@
+package lt.techin.booksproject.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 320)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false, length = 255)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Role role;
+}

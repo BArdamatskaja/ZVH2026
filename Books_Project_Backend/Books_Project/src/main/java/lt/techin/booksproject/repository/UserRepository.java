c30709ec5a87bf919a2c4be5b8f34a12b52790d0
Algimantas Vizgirda <algimantas.vizgirda@gmail.com>
2026-01-14 14:30:02 +0200

    feat(user): add UserRepository

---
+++ Books_Project_Backend/Books_Project/src/main/java/lt/techin/booksproject/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package lt.techin.booksproject.repository;
+
+import lt.techin.booksproject.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}

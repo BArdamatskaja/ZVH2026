a6e96bfad5f144663f868485a2b405af14e5d5cd
BArdamatskaja <malachovskaja123@gmail.com>
2026-01-21 00:27:58 +0200

    Create filter for books by title and category

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import Books from "../../components/Books";
 function Home() {
   return (
     <div>
       <h1>Home</h1>
       <p>Frontend scaffold is ready.</p>
+      <Books />
     </div>
   );
 }
